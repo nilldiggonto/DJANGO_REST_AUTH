27789b63163038400bed69c968258d44361a2681
nilldiggonto <nilldiggonto@gmail.com>
2020-11-20 06:45:35 +0600

    Api Setup Done

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,6 +6,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/',include('rest_framework.urls')),
+    path('api/',include('profiles.api.urls',namespace='profiles')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
